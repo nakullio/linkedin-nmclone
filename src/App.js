ae34100cccde26c266a2d5cd009433372a68a0aa
Nakula <nakulamarvellio@gmail.com>
2021-04-25 23:45:49 +0700

    ADD: Building the login page

--- src/App.js
+++ src/App.js
@@ -3,17 +3,29 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+import Login from './Login';
 
 function App() {
+  // pull user from data store
+  const user = useSelector(selectUser)
+
   return (
     <div className="app">
       <Header />
 
-      <div className="app__body">
-        <Sidebar />
-         <Feed />
-        {/* Widgets */}
-      </div>    
+      {!user ? (
+      <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          {/* Widgets */}
+        </div> 
+      )}
+
+         
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -20,7 +20,7 @@ function Header() {
                 <img src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" alt=""/>
                 <div className="header__search">
                     <SearchIcon />
-                    <input type="text"/>
+                    <input placeholder="Search" type="text"/>
                 </div>
 
             </div>
