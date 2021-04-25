504f24c642872dbd34eb264ae67296b2606a1503
Nakula <nakulamarvellio@gmail.com>
2021-04-25 20:51:29 +0700

    CHNG: finishing InputOption

---
+++ src/Feed.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Feed.css'
+import InputOption from './InputOption'
+import CreateIcon from '@material-ui/icons/Create'
+import ImageIcon from '@material-ui/icons/Image'
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions'
+import EventNoteIcon from '@material-ui/icons/EventNote'
+import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay'
+
+function Feed() {
+    return (
+        <div className="feed">
+            <div className="feed__inputContainer">
+                <div className="feed__input">
+                    <CreateIcon />
+                    <form>
+                        <input type="text"/>
+                        <button type='submit' >Send</button>
+                    </form>
+                </div>
+                <div className="feed__inputOptions">
+                    <InputOption Icon={ImageIcon} title='Photo' color='#70b5f9'/>
+                    <InputOption Icon={SubscriptionsIcon} title='Video' color='#e7a33e'/>
+                    <InputOption Icon={EventNoteIcon} title='Event' color='#c0cbcd'/>
+                    <InputOption Icon={CalendarViewDayIcon} title='Write Article' color='#7fc15e'/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Feed
